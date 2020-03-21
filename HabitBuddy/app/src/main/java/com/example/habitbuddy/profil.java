35b22002e7a4bcd44a41f192240abea85c84d190
thierry <thierry.changf@gmail.com>
2020-03-21 13:37:48 +0100

    Upgrade IMC

--- HabitBuddy/app/src/main/java/com/example/habitbuddy/profil.java
+++ HabitBuddy/app/src/main/java/com/example/habitbuddy/profil.java
@@ -2,6 +2,7 @@ package com.example.habitbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -49,9 +50,27 @@ public class profil extends AppCompatActivity {
         try {
             float IMC = (poi * 10000)/(taill * taill);
 
-            imc.setText(String.valueOf(IMC));
+            String votreIMC;
+            if (IMC<18){
+                votreIMC = "Vous êtes en sous poid";
+            }else if (IMC>25){
+                votreIMC = "Vous êtes en sur poid";
+            }else {
+                votreIMC = "Votre poid est normale";
+            }
+
+            imc.setText("Votre IMC est : "+IMC+"\n"+votreIMC);
         }catch (Exception e){
             imc.setText("Error : "+e);
         }
     }
+
+    /*private void graphique(){
+
+
+        Intent chartDroidIntent = new Intent(Intent.ACTION_VIEW, ChartDroidDataProvider.PROVIDER_URI);
+        chartDroidIntent.putExtra(Intent.EXTRA_TITLE, "Chart droid");
+        chartDroidIntent.addCategory("com.googlecode.chartdroid.intent.category.PIE_CHART");
+        startActivity(chartDroidIntent);
+    }*/
 }
